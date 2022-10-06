65d769587cb08dfb1c38ce67143874c0085bbc0d
Claudio Neto <claudio.ferreira.raizes@gmail.com>
2022-10-05 22:59:07 -0300

    fix parameter names

--- aurora-mysql-serverless-v2.cfhighlander.rb
+++ aurora-mysql-serverless-v2.cfhighlander.rb
@@ -3,7 +3,7 @@ CfhighlanderTemplate do
   Parameters do
     ComponentParam 'DBClusterIdentifier', ''
     ComponentParam 'MinCapacity', '1'
-    ComponentParam 'MinCapacity', '1'
+    ComponentParam 'MaxCapacity', '1'
   end
   
 end
